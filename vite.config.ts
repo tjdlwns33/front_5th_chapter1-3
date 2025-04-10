f988b594b61334bb25a1cf9f7004a3d0897b5797
tjdlwns33 <tjdlwns33@gmail.com>
2025-04-10 22:32:52 +0900

    배포관련 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+
+export const BASE_PATH = process.env.NODE_ENV === "production" ? "/front_5th_chapter1-3/" : "/";
+
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: BASE_PATH,
   }),
   defineTestConfig({
     test: {
